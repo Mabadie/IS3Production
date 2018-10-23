1b8b880a993dc847924025cefe5450a29a7cc471
Mariano <abadiemariano@gmail.com>
2018-10-23 01:02:10 -0300

    Static root implemented

--- sharebooks/settings.py
+++ sharebooks/settings.py
@@ -155,5 +155,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/media/'
+STATIC_ROOT = BASE_DIR+'/static/'
 ALLOWED_HOSTS = ['bookshare.com', 'localhost']
 MEDIA_URL = 'http://media.bookshare.com/'
