309d87f5e27f45faeb022f21b9a570ba100c9e8e
Mariano <abadiemariano@gmail.com>
2018-10-23 00:29:42 -0300

    Heroku changes, ignoring .pyc files now

--- bookshare/sharebooks/settings.py
+++ bookshare/sharebooks/settings.py
@@ -77,7 +77,17 @@ WSGI_APPLICATION = 'sharebooks.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookshare',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '5432',
+    }
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -88,7 +98,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
