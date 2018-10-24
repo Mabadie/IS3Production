75f2eea7f98d1f607d285b6cf28e87a40873dd7f
Mariano <abadiemariano@gmail.com>
2018-10-23 21:13:07 -0300

    Heroku base dir fix for whitenoise

--- sharebooks/wsgi.py
+++ sharebooks/wsgi.py
@@ -6,14 +6,17 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-
 import os
+
+
 
 from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sharebooks.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=BASEDIR+'/media')
+application = WhiteNoise(application, root=BASE_DIR+'/media')
