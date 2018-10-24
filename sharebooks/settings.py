7ba355efc5ac8c50fcb594361e5d751633416e82
Mariano <abadiemariano@gmail.com>
2018-10-23 21:02:11 -0300

    Heroku css fix

--- sharebooks/settings.py
+++ sharebooks/settings.py
@@ -154,8 +154,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/media/'
-STATIC_ROOT = BASE_DIR+'/media/'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'media')
 ALLOWED_HOSTS = ['bookshare1995.herokuapp.com', 'bookshare.com', 'localhost']
 MEDIA_URL = 'http://media.bookshare.com/'
