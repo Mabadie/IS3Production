11491e0e7e4d2278f67e257e85269f61511b129c
kbmaster <kbmaster231281@hotmail.com>
2018-11-06 17:41:36 -0300

    kk

--- app/config/web.config.js
+++ app/config/web.config.js
@@ -1,4 +1,4 @@
 angular.module('SHAREBOOKSApp').constant('APP_CONFIG', {
-    ct_urlBase: 'http://localhost:8000',
+    ct_urlBase: 'https://bookshare1995.herokuapp.com/',
     ct_urlService: ''
 });
