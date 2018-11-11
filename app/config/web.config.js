15120bdf92c15614ef32376a33e2de0614d28321
Mariano <abadiemariano@gmail.com>
2018-11-11 14:12:33 -0300

    Reverse configuration files

--- app/config/web.config.js
+++ app/config/web.config.js
@@ -1,5 +1,5 @@
 angular.module('SHAREBOOKSApp').constant('APP_CONFIG', {
-    //ct_urlBase: 'https://bookshare1995.herokuapp.com',
-    ct_urlBase: 'http://localhost:8000'	,
+    ct_urlBase: 'https://bookshare1995.herokuapp.com',
+    //ct_urlBase: 'http://localhost:8000'	,
     ct_urlService: ''
 });
